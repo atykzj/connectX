d4f399df179dc770eb79a6873a55e44041e35cd6
atykzj <61933159+atykzj@users.noreply.github.com>
2023-04-10 10:39:05 +0800

    a

---
+++ Agent/AB_agent_ScoreM.py
@@ -0,0 +1,178 @@
+
+################################
+# Imports and helper functions #
+################################
+
+import numpy as np
+import random   
+
+# scoring matrix
+score_mat = [
+    [3, 4, 5, 7, 5, 4, 3],
+    [4, 6, 8, 10, 8, 6, 4],
+    [5, 8, 11, 13, 11, 8, 5],
+    [5, 8, 11, 13, 11, 8, 5],
+    [4, 6, 8, 10, 8, 6, 4],
+    [3, 4, 5, 7, 5, 4, 3]
+]
+
+
+def AB_agent_ScoreM(obs, config):
+
+    # Trying with Alpha–beta pruning implementation. N_STEPS = 5 leads to timeout if submited without Alpha–beta pruning. 
+    N_STEPS = 3
+
+    # Helper function for score_move: gets board at next step if agent drops piece in selected column
+    def drop_piece(grid, col, mark, config):
+        next_grid = grid.copy()
+        for row in range(config.rows-1, -1, -1):
+            if next_grid[row][col] == 0:
+                break
+        next_grid[row][col] = mark
+        cell_state_rewards = score_mat[row][col]
+        return next_grid, cell_state_rewards
+
+    # Uses minimax to calculate value of dropping piece in selected column
+    def score_move(grid, col, mark, config, nsteps):
+        next_grid, cell_state_rewards = drop_piece(grid, col, mark, config)
+        alpha, beta = -1e4, 1e6
+        score = minimax(next_grid, nsteps-1, False, mark, alpha, beta, config)
+        return score
+
+    # Helper function for minimax: checks if agent or opponent has four in a row in the window
+    def is_terminal_window(window, config):
+        return window.count(1) == config.inarow or window.count(2) == config.inarow
+
+    # Helper function for minimax: checks if game has ended
+    def is_terminal_node(grid, config):
+        # Check for draw 
+        if list(grid[0, :]).count(0) == 0:
+            return True
+        # Check for win: horizontal, vertical, or diagonal
+        # horizontal 
+        for row in range(config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[row, col:col+config.inarow])
+                if is_terminal_window(window, config):
+                    return True
+        # vertical
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns):
+                window = list(grid[row:row+config.inarow, col])
+                if is_terminal_window(window, config):
+                    return True
+        # positive diagonal
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+                if is_terminal_window(window, config):
+                    return True
+        # negative diagonal
+        for row in range(config.inarow-1, config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+                if is_terminal_window(window, config):
+                    return True
+        return False
+
+    # # Minimax implementation with Alpha-Beta pruning
+    def minimax(node, depth, maximizingPlayer, mark, alpha, beta, config):
+        is_terminal = is_terminal_node(node, config)
+        valid_moves = [c for c in range(config.columns) if node[0][c] == 0]
+        if depth == 0 or is_terminal:
+            return get_heuristic(node, mark, config), None
+        if maximizingPlayer:
+            value = -np.Inf
+            # Add predefined score to value
+            move = None
+            for col in valid_moves:
+                child, cell_state_rewards = drop_piece(node, col, mark, config)
+                child_value, _ = minimax(child, depth-1, False, mark, alpha, beta, config)
+                # # Add state rewards
+                # child_value += cell_state_rewards
+                if child_value > value:
+                    value = child_value
+                    move = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return value, move
+        else:
+            value = np.Inf
+            move = None
+            for col in valid_moves:
+                child, cell_state_rewards = drop_piece(node, col, mark%2+1, config)
+                child_value, _ = minimax(child, depth-1, True, mark, alpha, beta, config)
+                # # Add state rewards
+                # child_value -= cell_state_rewards
+                if child_value < value:
+                    value = child_value
+                    move = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return value, move
+
+
+    # Helper function for minimax: calculates value of heuristic for grid
+    def get_heuristic(grid, mark, config):
+        num_threes = count_windows(grid, 3, mark, config)
+        num_fours = count_windows(grid, 4, mark, config)
+        num_threes_opp = count_windows(grid, 3, mark%2+1, config)
+        num_fours_opp = count_windows(grid, 4, mark%2+1, config)
+        score = num_threes - 1e2*num_threes_opp - 1e4*num_fours_opp + 1e6*num_fours
+        return score
+
+    # Helper function for get_heuristic: checks if window satisfies heuristic conditions
+    def check_window(window, num_discs, piece, config):
+        return (window.count(piece) == num_discs and window.count(0) == config.inarow-num_discs)
+
+    # Helper function for get_heuristic: counts number of windows satisfying specified heuristic conditions
+    def count_windows(grid, num_discs, piece, config):
+        num_windows = 0
+        # horizontal
+        for row in range(config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[row, col:col+config.inarow])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        # vertical
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns):
+                window = list(grid[row:row+config.inarow, col])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        # positive diagonal
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        # negative diagonal
+        for row in range(config.inarow-1, config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        return num_windows
+
+    # Get list of valid moves
+    valid_moves = [c for c in range(config.columns) if obs.board[c] == 0]
+    # Convert the board to a 2D grid
+    grid = np.asarray(obs.board).reshape(config.rows, config.columns)
+    # Score Matrix
+    curr_cell_state_rewards = {}
+    for col in valid_moves:
+        curr_cell_state_rewards[col] = 0
+        for row in range(config.rows-1, -1, -1):
+            if grid[row][col] == 0 and curr_cell_state_rewards[col]==0:
+                curr_cell_state_rewards[col] = score_mat[row][col]
+
+    scores = dict(zip(valid_moves, [score_move(grid, col, obs.mark, config, N_STEPS) for col in valid_moves]))
+    # Use the heuristic to assign a score to each possible board in the next step
+    # scores = dict(zip(valid_moves, [score_move(grid, valid_moves[col], obs.mark, config, N_STEPS) + \
+    #                                  curr_cell_state_rewards[col] for col in valid_moves]))
+    # Get a list of columns (moves) that maximize the heuristic
+    max_cols = [key for key in scores.keys() if scores[key] == max(scores.values())]
+    # Select at random from the maximizing columns
+    return random.choice(max_cols)
